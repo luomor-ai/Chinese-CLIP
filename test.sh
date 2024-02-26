5407543ce1d9784c549976d28e93c296caa77259
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-02-26 19:01:26 +0800

    update

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+# Usage: extract image and text features
+# only supports single-GPU inference
+export CUDA_VISIBLE_DEVICES=${1}
+export PYTHONPATH=${PYTHONPATH}:`pwd`/cn_clip
+ 
+SPLIT=${2}
+DATAPATH=${3}
+RESUME=${DATAPATH}/pretrained_weights/clip_cn_rn50.pt
+DATASET_NAME=Flickr30k-CN
+ 
+python -u cn_clip/eval/extract_features.py \
+    --extract-image-feats \
+    --extract-text-feats \
+    --image-data="${DATAPATH}/datasets/${DATASET_NAME}/lmdb/${SPLIT}/imgs" \
+    --text-data="${DATAPATH}/datasets/${DATASET_NAME}/${SPLIT}_texts.jsonl" \
+    --img-batch-size=32 \
+    --text-batch-size=32 \
+    --context-length=52 \
+    --resume=${RESUME} \
+    --vision-model=RN50 \
+    --text-model=RBT3-chinese
